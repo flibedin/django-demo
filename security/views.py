a13419e01f36fcd29df70af64b24b1d981484860
Fernando Libedinsky <fernando@MacBook.local>
2018-10-18 13:31:17 -0300

    .

--- security/views.py
+++ security/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
 import json
-import urllib
+import urllib.request
 
 
 
